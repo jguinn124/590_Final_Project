8608217d6458a570e89073fa740b56b5ab0982ef
jguinn124 <44623156+jguinn124@users.noreply.github.com>
2019-05-08 12:40:10 -0500

    Add files via upload

--- 590_final.py
+++ 590_final.py
@@ -4,171 +4,106 @@ John Guinn
 
 nmap GUI
 """
+from tkinter import *
 import tkinter as tk
 import time
 import threading
 from queue import Queue
 import socket
-import sys
-from datetime import datetime
-#import nmap
 
 class Application(tk.Frame):
-	def __init__(self, master=None):
-		super().__init__(master)
-		self.master = master
-		master.title("Scanner GUI")
-		self.pack()
-		self.create_widgets()
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        master.title("Scanner GUI")
+        master.geometry("300x300")
+        self.pack()
+        self.create_widgets()
+        
 
-	def create_widgets(self):
-		self.hi_there = tk.Button(self)
-		self.hi_there["text"] = "Hello World\n(click me)"
-		self.hi_there["command"] = self.say_hi
-		self.hi_there.pack(side="top")
-		
-		self.p_scan = tk.Button(self, text="Scan Ports", fg="blue", command=self.p_scan)
-		self.p_scan.pack()
-		
-		self.p_scan = tk.Button(self, text="Scan External Ports", fg="blue", command=self.p_scan)
-		self.p_scan.pack()
-		
-		self.arp_cache = tk.Button(self, text="ARP cache", fg="blue", command=self.arp_cache)
-		self.arp_cache.pack()
-		
-		#self.nmap_scan = tk.Button(self, text="Nmap Scan", fg="blue", command=self.nmap_scan)
-		#self.nmap_scan.pack()
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Documentation"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+        
 
-		self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-		self.quit.pack(side="bottom")
-		
+        
+        self.p_scan = tk.Button(self, text="Scan Local Ports", fg="blue", command=self.p_scan)
+        self.p_scan.pack()
 
-	def say_hi(self):
-		print("hi there, everyone!")
+        self.p_scan = tk.Button(self, text="Scan External Ports", fg="blue", command=self.p_scan)
+        self.p_scan.pack()
+        
+        self.arp_cache = tk.Button(self, text="ARP cache", fg="blue", command=self.arp_cache)
+        self.arp_cache.pack()
 
-	def scan_window(seft):
-		top = Toplevel()
-		top.title("About this application...")
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
 
-		msg = Message(top, text=about_message)
-		msg.pack()
+    def say_hi(self):
+        print("hi there, everyone!")
 
-		button = Button(top, text="Dismiss", command=top.destroy)
-		button.pack()
-		
-	
-	def p_scan(self):
-		import subprocess
-		
-		
-		top = Toplevel()
-		top.title("Scanning results")
-		top.minsize(300, 300)
+    def scan_window(seft):
+        top = Toplevel()
+        top.title("About this application...")
+
+        msg = Message(top, text=about_message)
+        msg.pack()
+
+        button = Button(top, text="Dismiss", command=top.destroy)
+        button.pack()
+        
+    
+    def p_scan(self):
+        
+        print("Scanning...")
 
 
-		
-		# put results in data
-		data = 5
 
-		label = Message(top, text="Data \n")
-		label.pack()
+        top = Toplevel()
+        top.title("Scanning results")
+        top.minsize(300, 300)
 
-		label = Message(top, text=data)
-		label.pack()
-		
-		
 
-		button = Button(top, text="Dismiss", command=top.destroy)
-		button.pack()
-		print("Scanning...")
-		
-		subprocess.call('clear', shell=True)
-		
-		remoteServer = input("Enter a remote host to scan: ")
-		remoteServerIP = socket.gethostbyname(remoteServer)
-		
-		
+        
+        # put results in data
+        data = 5
 
-		time.sleep(1)
+        label = Message(top, text="Data \n")
+        label.pack()
 
-		print("-"*60)
-		
-		time.sleep(1)
-   
+        label = Message(top, text=data)
+        label.pack()
+        
+        
 
-		print("Please wait, scanning remote host", remoteServerIP) 
-		
+        button = Button(top, text="Dismiss", command=top.destroy)
+        button.pack()
 
-		
-		print("-"*60)
-		
-		t1 = datetime.now()
-		
-		try:
-			for port in range(1,1025):
-				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				result = sock.connect_ex((remoteServerIP, port))
-				if result == 0:
-					print("Port {}:		Open".format(port))
-				sock.close()
-		
-		except KeyboardInterrupt:
-			print("You pressed Ctrl+C")
-			sys.exit()
-		
-		except socket.gaierror:
-			print("Hostname could not be resolved. Exiting")
-			sys.exit()
-		
-		except socket.error:
-			print("Could not connect to server")
-			sys.exit()
-			
-		t2 = datetime.now()
-		
-		total = t1 - t2
-		
-		print("Scanning Completed in: ", total)
-		
-	def arp_cache(self):
-		
-		print(" Arp stuff")
-		import subprocess
-		#subprocess.check_output(['ls','-l']) #all that is technically needed...
-		
-		ls = subprocess.check_output(['ls'])
-		
-		a = "\\"
-		
-		
-		str(ls)
-		
-		ls.split(a)
-		
-		print(ls)
-		
-	'''def nmap_scan(self):
-		nm = nmap.PortScanner()
-		nm.scan('127.0.0.1', '20-1024')
-		print(nm.command_line())
-		
-		for host in nm.all_hosts():
-			print('----------------------------------------------------')
-			print('Host : {} ({})'.format(host, nm[host].hostname()))
-			print('State : {}'.format(nm[host].state()))
-		for proto in nm[host].all_protocols():
-			print('---------')
-			print('Protocol : {}'.format(proto))
-		
-		lport = nm[host][proto].keys()
-		for port in lport:
-			print('port : {}\tstate : {}'.format(port, nm[host][proto][port]['state']))'''
-			
+                
 
-		
-		
-	
-		
+        
+    def arp_cache(self):
+        
+        print(" Arp stuff")
+        import subprocess
+        #subprocess.check_output(['ls','-l']) #all that is technically needed...
+        
+        ls = subprocess.check_output(['ls'])
+        
+        a = "\\"
+        
+        
+        str(ls)
+        
+        ls.split(a)
+        
+        print(ls)
+        
+        
+    
+        
 
 root = tk.Tk()
 app = Application(master=root)
