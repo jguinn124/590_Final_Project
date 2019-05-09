a07949cdfcd127d6507f37676da045ce12358ff7
devencolbert <31320471+devencolbert@users.noreply.github.com>
2019-05-09 16:51:46 -0500

    Update 590_final.py

--- 590_final.py
+++ 590_final.py
@@ -19,7 +19,7 @@ class Application(tk.Frame):
 		super().__init__(master)
 		self.master = master
 		master.title("Easy Network Utility")
-		master.geometry("300x300")
+		master.geometry("600x600")
 		self.pack()
 		self.create_widgets()
 		self.target = 'hackthissite.org'
@@ -31,6 +31,11 @@ class Application(tk.Frame):
 		self.hi_there["text"] = "Hello World\n(click me)"
 		self.hi_there["command"] = self.say_hi
 		self.hi_there.pack(side="top")
+		
+		self.E1 = Entry(self, bd =5)
+		self.E1.pack(side = "top")
+
+		print(self.E1.get())
 		
 		self.p_scan = tk.Button(self, text="Scan Ports", fg="blue", command=self.p_scan)
 		self.p_scan.pack()
@@ -53,6 +58,15 @@ class Application(tk.Frame):
 		self.netstat_button = tk.Button(self, text="Router Configuration", fg="blue", command=self.netstat)
 		self.netstat_button.pack()
 		
+		self.arp_button = tk.Button(self, text="ARP Cache", fg="blue", command=self.arp_cache)
+		self.arp_button.pack()
+
+		self.ip_button = tk.Button(self, text="My IP Address", fg="blue", command=self.my_ip)
+		self.ip_button.pack()
+
+		self.dns_button = tk.Button(self, text="My Hostname and Domain", fg="blue", command=self.my_DNS)
+		self.dns_button.pack()
+		
 		#self.nmap_scan = tk.Button(self, text="Nmap Scan", fg="blue", command=self.nmap_scan)
 		#self.nmap_scan.pack()
 
@@ -61,7 +75,31 @@ class Application(tk.Frame):
 		
 
 	def say_hi(self):
-		print("hi there, everyone!")
+		top = Toplevel()
+		top.title("Documentation")
+		top.minsize(300, 300)
+			
+			# put results in data
+		data = """       Do not use this application to attack machines that you have no authourity over.
+		Scan Ports button (requires address)
+		The scanning ports button will allow you to see open ports on your machine if you use localhost or find open ports on other machines
+		
+		Ping button (requires address)
+		This ping button will allow you to test the connection between your machine and another machine.
+		Network Configuration button
+		The Network Configuration button will allow you to see information about the network you are connected to.
+		Router Configuration Button
+		The Router Configuration Button will allow you to see how your router is configured and information about it.
+		ARP Cashe button
+		
+		
+		For this programming project, we built a tkinter GUI application in python that allowed allows the user to scan the ports on their remote machine, ping the remote machine or a user inputted address, a configuration option to get the ip address of your machine and a netstat option to get the network statistics of you machine. These are the features that we have implemented for our project. The program is built in python as a class and each feature is a method in the class called “Application”. Each feature of this project is a button on the application GUI.
+			"""
+
+
+		label = Text(top)
+		label.pack()
+		label.insert(END, data)
 
 	def scan_window(seft):
 		top = Toplevel()
@@ -83,8 +121,8 @@ class Application(tk.Frame):
 		
 		subprocess.call('clear', shell=True)
 		
-		remoteServer = input("Enter a remote host to scan: ")
-		remoteServerIP = socket.gethostbyname(remoteServer)
+		remoteServer    = self.E1.get()
+		remoteServerIP  = socket.gethostbyname(remoteServer)
 		
 		
 
@@ -93,7 +131,6 @@ class Application(tk.Frame):
 		print("-"*60)
 		
 		time.sleep(1)
-   
 
 		print("Please wait, scanning remote host", remoteServerIP) 
 		
@@ -136,7 +173,7 @@ class Application(tk.Frame):
 
 		
 		# put results in data
-		data = 5
+		data = open_port
 
 		label = Message(top, text="Data \n")
 		label.pack()
@@ -148,15 +185,35 @@ class Application(tk.Frame):
 
 		button = Button(top, text="Dismiss", command=top.destroy)
 		button.pack()
+
+		
+		
+
+		button = Button(top, text="Dismiss", command=top.destroy)
+		button.pack()
 		
 		
 	def scan(self, port):
 		
+		top = Toplevel()
+		top.title("Scanning results")
+		top.minsize(300, 300)
+		
+		T = Text(top, height=30, width=100)
+		scrollbar = Scrollbar(top)
+		scrollbar.pack( side = RIGHT, fill = Y )
+		T.pack(side=LEFT, fill=Y)
+		scrollbar.config( command = T.yview )
+		T.config(yscrollcommand=scrollbar.set)
+		
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
-			con = sock.connect((self.target,port))
+			con = sock.connect((self.E1.get(),port))
 			with self.print_lock:
-				print('port', port)
+				data = res
+		
+				T.insert(END, data)
+				#print('port', port + '		Open')
 			con.close()
 		except:
 			pass
@@ -172,10 +229,10 @@ class Application(tk.Frame):
 	def port_scan(self):
 		
 		for i in range(30):
-			t = threading.Thread(target=self.threader)
+			t = threading.Thread(target=self.threader).start()
 			
-			t.daemon = True
-			t.start()
+			#t.daemon = True
+			#t.start()
 			
 		start = time.time()
 		
@@ -276,6 +333,86 @@ class Application(tk.Frame):
 		data = res
 		
 		T.insert(END, data)
+		
+	def arp_cache(self):
+		
+		import subprocess
+		#subprocess.check_output(['ls','-l']) #all that is technically needed...
+		res = subprocess.check_output(["arp", "-a"])
+
+		top = Toplevel()
+		top.title("Results")
+		top.minsize(500, 100)
+
+		
+			
+
+		# put results in data
+		data = res
+		label = Message(top, text="Data \n")
+		label.pack()
+
+		label = Text(top)
+		label.pack()
+		label.insert(END, data)
+
+
+		scroll1y=Scrollbar(top, command=label.yview)
+		scroll1y.pack(side=LEFT, fill=Y)
+
+
+	def my_ip(self):
+		
+		import subprocess
+		#subprocess.check_output(['ls','-l']) #all that is technically needed...
+		res = subprocess.check_output(["curl", "ifconfig.me"])
+
+		top = Toplevel()
+		top.title("Results")
+		top.minsize(100, 100)
+
+		
+			
+
+		# put results in data
+		data = res
+		label = Message(top, text="Data \n")
+		label.pack()
+
+		label = Text(top)
+		label.pack()
+		label.insert(END, data)
+
+
+		scroll1y=Scrollbar(top, command=label.yview)
+		scroll1y.pack(side=LEFT, fill=Y)
+
+
+	def my_DNS(self):
+		
+		import subprocess
+		#subprocess.check_output(['ls','-l']) #all that is technically needed...
+		res = subprocess.check_output(["hostname"])
+
+		top = Toplevel()
+		top.title("Results")
+		top.minsize(100, 100)
+
+		
+			
+
+		# put results in data
+		data = res
+		label = Message(top, text="Data \n")
+		label.pack()
+
+		label = Text(top)
+		label.pack()
+		label.insert(END, data)
+
+
+		scroll1y=Scrollbar(top, command=label.yview)
+		scroll1y.pack(side=LEFT, fill=Y)
 		
 		
 	'''def nmap_scan(self):
