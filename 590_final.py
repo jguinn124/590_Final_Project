def240782c7ad2912ad3ddb673ca0a3fac83d1fe
jguinn124 <44623156+jguinn124@users.noreply.github.com>
2019-05-09 15:48:18 -0500

    Its done
    Every network command is on there basically

--- 590_final.py
+++ 590_final.py
@@ -16,7 +16,7 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        master.title("Easy Network Utility")
+        master.title("Network Utility")
         master.geometry("300x300")
         self.pack()
         self.create_widgets()
@@ -26,7 +26,7 @@ class Application(tk.Frame):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Documentation"
         self.hi_there["command"] = self.say_hi
-        #self.hi_there.pack(side="top")
+        self.hi_there.pack()
 
 
         
@@ -37,17 +37,14 @@ class Application(tk.Frame):
         
 
         
-        self.p_scan = tk.Button(self, text="Scan Local Ports", fg="blue", command=self.p_scan)
-        self.p_scan.pack()
 
-        self.p_scan = tk.Button(self, text="Scan External Ports", fg="blue", command=self.p_scan)
+
+        self.p_scan = tk.Button(self, text="Scan Ports", fg="blue", command=self.p_scan)
         self.p_scan.pack()
         
-        self.ping = tk.Button(self, text="Ping local", fg="blue", command=self.ping)
+        self.ping = tk.Button(self, text="Ping", fg="blue", command=self.ping)
         self.ping.pack()
 
-        self.ping_button = tk.Button(self, text="Ping out", fg="blue", command=self.ping_out)
-        self.ping_button.pack()
 
         self.ifcon_button = tk.Button(self, text="Network Configuration", fg="blue", command=self.ifconfig)
         self.ifcon_button.pack()
@@ -55,23 +52,49 @@ class Application(tk.Frame):
         self.netstat_button = tk.Button(self, text="Router Configuration", fg="blue", command=self.netstat)
         self.netstat_button.pack()
 
+        self.arp_button = tk.Button(self, text="ARP Cache", fg="blue", command=self.arp_cache)
+        self.arp_button.pack()
+
+        self.ip_button = tk.Button(self, text="My IP Address", fg="blue", command=self.my_ip)
+        self.ip_button.pack()
+
+        self.dns_button = tk.Button(self, text="My Hostname and Domain", fg="blue", command=self.my_DNS)
+        self.dns_button.pack()
+
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
 
     def say_hi(self):
         top = Toplevel()
-        top.title("Scanning results")
+        top.title("Documentation")
         top.minsize(300, 300)
             
             # put results in data
-        data = """ Blah Blah dont use this application to attack machines that you have no authourity over.
+        data = """       Do not use this application to attack machines that you have no authourity over.
+
+        Scan Ports button (requires address)
+        The scanning ports button will allow you to see open ports on your machine if you use localhost or find open ports on other machines
+        
+        Ping button (requires address)
+        This ping button will allow you to test the connection between your machine and another machine.
+
+        Network Configuration button
+        The Network Configuration button will allow you to see information about the network you are connected to.
+
+        Router Configuration Button
+        The Router Configuration Button will allow you to see how your router is configured and information about it.
+
+        ARP Cashe button
+        
+        
+        For this programming project, we built a tkinter GUI application in python that allowed allows the user to scan the ports on their remote machine, ping the remote machine or a user inputted address, a configuration option to get the ip address of your machine and a netstat option to get the network statistics of you machine. These are the features that we have implemented for our project. The program is built in python as a class and each feature is a method in the class called “Application”. Each feature of this project is a button on the application GUI.
+
             """
 
-        label = Message(top, text="Documentation")
-        label.pack()
 
-        label = Message(top, text=data)
+        label = Text(top)
         label.pack()
+        label.insert(END, data)
 
 
         
@@ -90,7 +113,7 @@ class Application(tk.Frame):
     def p_scan(self):
 
         print_lock = threading.Lock()
-        remoteServer    = "localhost"
+        remoteServer    = self.E1.get()
         remoteServerIP  = socket.gethostbyname(remoteServer)
 
 
@@ -156,34 +179,14 @@ class Application(tk.Frame):
         
 
 
-    def ping_out(self):
-        
-        print("pinging")
-        import subprocess
-        #subprocess.check_output(['ls','-l']) #all that is technically needed...
-        addr = input("where to?")
-        ping = subprocess.check_output(["ping", addr, "-c 5"])
 
-        top = Toplevel()
-        top.title("Ping results")
-        top.minsize(300, 300)
-            
-
-        # put results in data
-        data = ping
-
-        label = Message(top, text="Data \n")
-        label.pack()
-
-        label = Message(top, text=data)
-        label.pack()
 
 
     def ifconfig(self):
         
         import subprocess
         #subprocess.check_output(['ls','-l']) #all that is technically needed...
-        res = subprocess.check_output(["ifconfig"])
+        res = subprocess.check_output(["ifconfig", "-a"])
 
         top = Toplevel()
         top.title("Results")
@@ -194,15 +197,16 @@ class Application(tk.Frame):
 
         # put results in data
         data = res
+        label = Message(top, text="Data \n")
+        label.pack()
 
         label = Text(top)
         label.pack()
-        label.insert(END, "Data \n")
         label.insert(END, data)
 
 
         scroll1y=Scrollbar(top, command=label.yview)
-        scroll1y.pack(side=LEFT, fill=Y, pady=65)
+        scroll1y.pack(side=LEFT, fill=Y)
 
     def netstat(self):
         
@@ -215,9 +219,6 @@ class Application(tk.Frame):
         top.minsize(600, 300)
 
 
-        sc = Scrollbar(top, command=top.yview)
-        sc.pack(side=RIGHT, fill = Y)
-        #sc.config(command=top.yview)
             
 
         # put results in data
@@ -226,9 +227,92 @@ class Application(tk.Frame):
         label = Message(top, text="Data \n")
         label.pack()
 
-        label = Message(top, text=data)
+        label = Text(top)
         label.pack()
+        label.insert(END, data)
+
+        scroll1y=Scrollbar(top, command=label.yview)
+        scroll1y.pack(side=LEFT, fill=Y)
+
+    def arp_cache(self):
+        
+        import subprocess
+        #subprocess.check_output(['ls','-l']) #all that is technically needed...
+        res = subprocess.check_output(["arp", "-a"])
+
+        top = Toplevel()
+        top.title("Results")
+        top.minsize(500, 100)
+
+        
             
+
+        # put results in data
+        data = res
+        label = Message(top, text="Data \n")
+        label.pack()
+
+        label = Text(top)
+        label.pack()
+        label.insert(END, data)
+
+
+        scroll1y=Scrollbar(top, command=label.yview)
+        scroll1y.pack(side=LEFT, fill=Y)
+
+
+    def my_ip(self):
+        
+        import subprocess
+        #subprocess.check_output(['ls','-l']) #all that is technically needed...
+        res = subprocess.check_output(["curl", "ifconfig.me"])
+
+        top = Toplevel()
+        top.title("Results")
+        top.minsize(100, 100)
+
+        
+            
+
+        # put results in data
+        data = res
+        label = Message(top, text="Data \n")
+        label.pack()
+
+        label = Text(top)
+        label.pack()
+        label.insert(END, data)
+
+
+        scroll1y=Scrollbar(top, command=label.yview)
+        scroll1y.pack(side=LEFT, fill=Y)
+
+
+    def my_DNS(self):
+        
+        import subprocess
+        #subprocess.check_output(['ls','-l']) #all that is technically needed...
+        res = subprocess.check_output(["hostname"])
+
+        top = Toplevel()
+        top.title("Results")
+        top.minsize(100, 100)
+
+        
+            
+
+        # put results in data
+        data = res
+        label = Message(top, text="Data \n")
+        label.pack()
+
+        label = Text(top)
+        label.pack()
+        label.insert(END, data)
+
+
+        scroll1y=Scrollbar(top, command=label.yview)
+        scroll1y.pack(side=LEFT, fill=Y)
         
     
         
